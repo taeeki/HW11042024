ac316573298759fc27e6b4dc127993da3379414e
taeekii <terenteva1999@yandex.ru>
2024-04-14 13:12:36 +0300

    hw2

---
+++ 2/TreatmentPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treatment
+{
+    internal class TreatmentPlan
+    {
+        int code;
+        public int Code { get => code; }
+        public TreatmentPlan(int code)
+        {
+            this.code = code;
+        }
+    }
+}
